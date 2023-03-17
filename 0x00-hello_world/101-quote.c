219e5137f4887c1475e79dec345b00c8c45e639a
asofeyeje <drtunjiosungbade@gmail.com>
2023-03-16 21:53:29 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints a line to the standard error
- * Return 1
+ * main - A program that prints a line to the standard error
+ *
+ * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout");
+return (1);
 }
 
